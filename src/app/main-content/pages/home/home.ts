b3b55a48755c21242ffe976b8246903d6bc389d3
Eduard-Mheryan <eduardmheryan10@gmail.com>
2026-03-13 17:15:52 +0100

    Dein Commit-Text hier

--- src/app/main-content/pages/home/home.ts
+++ src/app/main-content/pages/home/home.ts
@@ -1,9 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnInit {
+
+  slides = [
+    {
+      image: '/images/cafe1.jpg',
+      title: 'Willkommen im Café',
+      text: 'Genieße unseren frischen Kaffee und hausgemachte Spezialitäten.'
+    },
+    {
+      image: '/images/cafe2.jpg',
+      title: 'Frisch gerösteter Kaffee',
+      text: 'Unsere Bohnen werden täglich frisch gemahlen.'
+    },
+    {
+      image: '/images/cafe3.jpg',
+      title: 'Hausgemachte Kuchen',
+      text: 'Perfekt zu einem Cappuccino am Nachmittag.'
+    },
+    {
+      image: '/images/cafe1.jpg',
+      title: 'Willkommen im Café',
+      text: 'Genieße unseren frischen Kaffee und hausgemachte Spezialitäten.'
+    },
+    {
+      image: '/images/cafe2.jpg',
+      title: 'Frisch gerösteter Kaffee',
+      text: 'Unsere Bohnen werden täglich frisch gemahlen.'
+    },
+    {
+      image: '/images/cafe3.jpg',
+      title: 'Hausgemachte Kuchen',
+      text: 'Perfekt zu einem Cappuccino am Nachmittag.'
+    }
+  ];
+
+  currentIndex = 0;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
+}
